573d366b70e092672cdb99433a0f7220a4956d43
QiQiao <qiqiao@SC02CF6AKMD6N.local>
2020-05-11 11:51:06 +0800

    fix: 修复依赖库及eslint规则问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 /**
  * eslint 规则配置文件，由 inithusky 自动产出
- * 默认采用腾讯腾讯代码规范Oteam指定规则包，各团队可根据实际情况自行修改扩充规则
- * http://tnpm.oa.com/package/@tencent/eslint-config-tencent
+ * 默认采用 airbnb 规则包，各团队可根据实际情况自行修改扩充规则
  */
+
 module.exports = {
-  extends: ['@tencent/eslint-config-tencent'],
-};
+  "parser": "babel-eslint",
+  "extends": "airbnb-base",
+}
